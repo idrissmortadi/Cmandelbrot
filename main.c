57549a18f9d995b3e03c4595e500e2e726e0f96c
Idriss Mo <idrissmortadi@gmail.com>
2024-08-09 19:46:50 +0100

    Fix main: use heap for image

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
-#include <stdint.h>
-#include <stdlib.h>
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define IMAGE_W 4096
 #define IMAGE_H 4096
@@ -17,10 +18,8 @@ typedef struct {
 } Complex;
 
 Complex pixels_to_complex(int i, int j) {
-  Complex z = {
-      SCALE * ((double)j / IMAGE_W - 0.5),
-      SCALE * ((double)i / IMAGE_H - 0.5) // Fix the calculation for y
-  };
+  Complex z = {SCALE * ((double)j / IMAGE_W - 0.5),
+               SCALE * ((double)i / IMAGE_H - 0.5)};
   return z;
 }
 
@@ -37,8 +36,12 @@ Complex multiply(Complex z1, Complex z2) {
 double magnitude(Complex z) { return z.x * z.x + z.y * z.y; }
 
 int main() {
-  // Use uint8_t for image data (0-255 range)
-  uint8_t img[IMAGE_H][IMAGE_W][3]; // RGB image (3 channels)
+  // Allocate memory for the image dynamically
+  uint8_t *img = malloc(IMAGE_W * IMAGE_H * 3 * sizeof(uint8_t));
+  if (img == NULL) {
+    perror("Failed to allocate memory");
+    return EXIT_FAILURE;
+  }
 
   for (int i = 0; i < IMAGE_H; i++) {
     for (int j = 0; j < IMAGE_W; j++) {
@@ -51,17 +54,19 @@ int main() {
         n++;
       }
 
-      // Map the number of iterations to a color
-      // Simple grayscale coloring
+      // Map the number of iterations to a color (grayscale)
       uint8_t color = (uint8_t)(255 * n / MAX_ITER);
-      img[i][j][0] = color; // Red
-      img[i][j][1] = color; // Green
-      img[i][j][2] = color; // Blue
+      img[(i * IMAGE_W + j) * 3 + 0] = color; // Red
+      img[(i * IMAGE_W + j) * 3 + 1] = color; // Green
+      img[(i * IMAGE_W + j) * 3 + 2] = color; // Blue
     }
   }
 
   // Save the image
   stbi_write_png("mandelbrot.png", IMAGE_W, IMAGE_H, 3, img, IMAGE_W * 3);
+
+  // Free the allocated memory
+  free(img);
 
   return EXIT_SUCCESS;
 }
